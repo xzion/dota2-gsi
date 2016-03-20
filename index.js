584621bab07257117a5497c1ccc8c49c376ca249
Coen McClelland <coen.mcclelland@gmail.com>
2016-03-20 18:03:40 +1100

    Basic server initialisation

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+var express         = require('express');
+var bodyParser      = require('body-parser');
+var eventEmitter    = require('events').EventEmitter;
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var events = new eventEmitter();
+
+function newData(req, res) {
+    events.emit('all:gamestate', req.body);
+    res.end();
+}
+
+var d2gsi = function(port) {
+    port = port || 3000;
+
+    app.post('/', newData);
+
+    var server = app.listen(port, function() {
+        console.log('Dota 2 GSI listening on port ' + server.address().port);
+    });
+
+    this.events = events;
+    return this;
+}
+
+module.exports = d2gsi;
