05ec8a5a4106b2be6e727101b94f87a1b6db9b97
xzion <coen.mcclelland@gmail.com>
2016-03-28 19:08:08 +1100

    Tweaked the test script

--- example/test.js
+++ example/test.js
@@ -3,7 +3,7 @@ var d2gsi = require('../index.js');
 var server = new d2gsi();
 
 server.events.on('newclient', function(client) {
-    console.log('New connection: ', client.ip);
+    console.log("New client connection, IP address: " + client.ip + ", Auth token: " + client.auth);
 
     client.on('hero:name', function(hero_name) {
         console.log("new emit from hero:name - " + hero_name);
